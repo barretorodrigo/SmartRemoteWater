b3d88bd290ff51be9d40b59914592e1170993c26
barretorodrigo <barretorodrigo@gmail.com>
2018-12-01 10:00:10 -0200

    Dados e Screenshots

--- arduino/script-to-import-csv.py
+++ arduino/script-to-import-csv.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('../data/26-10-18-1.csv', 'rb') as f:
+with open('../data/26-10-18-2.csv', 'rb') as f:
     reader = csv.reader(f, delimiter=';')
     for row in reader:
         print(', '.join(row))
